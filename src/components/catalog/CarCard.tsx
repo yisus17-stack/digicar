220f316e274a791aefbcd8bbc5af5e22c5a3e903
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2025-11-04 23:46:39 +0000

    no se arreglo jajajjaaj

--- src/components/catalog/CarCard.tsx
+++ src/components/catalog/CarCard.tsx
@@ -52,7 +52,7 @@ export default function CarCard({ car }: CarCardProps) {
           </div>
           <div className="flex items-center gap-2">
             <Users className="w-5 h-5" />
-            <span className="whitespace-nowrap">{car.passengers} Pasajeros</span>
+            <span className="whitespace-nowrap">{car.passengers} Pax.</span>
           </div>
         </div>
       </CardContent>
