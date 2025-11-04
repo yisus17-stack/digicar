d5be79fb920878cf17af4ee85aef30127cc06293
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2025-11-04 20:46:46 +0000

    ahora si quieres en la de inicio quita lo que dice chatbot jaj

--- src/components/assistant/VirtualAssistant.tsx
+++ src/components/assistant/VirtualAssistant.tsx
@@ -7,7 +7,6 @@ export default function VirtualAssistant() {
   return (
     <Button variant="ghost" className="flex items-center gap-2 text-primary hover:text-primary text-lg">
         <Bot className="h-8 w-8 p-1.5 border-2 border-primary rounded-full" />
-        <span className="font-bold">Chatbot</span>
     </Button>
   );
 }
