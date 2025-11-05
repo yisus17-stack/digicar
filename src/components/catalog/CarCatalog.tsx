0aa997aefb400d87ab4a0e0bb453808ac403e44c
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2025-11-05 00:58:22 +0000

    en el inicio pon en el celular las mismas cards que en categorias

--- src/components/catalog/CarCatalog.tsx
+++ src/components/catalog/CarCatalog.tsx
@@ -2,21 +2,28 @@
 
 import { Car } from '@/lib/types';
 import CarCard from './CarCard';
+import CarCardMobile from './CarCardMobile';
 
 interface CarCatalogProps {
   cars: Car[];
 }
 
-// A simplified version of CarCatalog for the home page.
 export default function CarCatalog({ cars }: CarCatalogProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {cars.map(car => (
-        <CarCard
-          key={car.id}
-          car={car}
-        />
-      ))}
-    </div>
+    <>
+      {/* Mobile View */}
+      <div className="grid grid-cols-1 gap-6 md:hidden">
+        {cars.map(car => (
+          <CarCardMobile key={`mobile-${car.id}`} car={car} />
+        ))}
+      </div>
+
+      {/* Desktop View */}
+      <div className="hidden md:grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {cars.map(car => (
+          <CarCard key={`desktop-${car.id}`} car={car} />
+        ))}
+      </div>
+    </>
   );
 }
