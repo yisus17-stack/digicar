b4ff10ea660d021d79f23e0fffc90746bee185d2
yisus17-stack <esquivelyisus17@gmail.com>
2025-11-11 20:38:51 +0000

    Try fixing this error: `Runtime Error: Error: Module [project]/node_modu

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -32,4 +30,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
