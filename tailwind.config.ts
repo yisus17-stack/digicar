a710cb987df9f0a636fea9cdfcb9d7b8614dd809
yisus17-stack <esquivelyisus17@gmail.com>
2025-12-06 19:02:05 +0000

    no se muestran en serio porque sera?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,15 +77,15 @@ export default {
             height: '0',
           },
         },
-        'marquee': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-50%)' },
-        }
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 40s linear infinite',
+        marquee: 'marquee 40s linear infinite',
       },
     },
   },
