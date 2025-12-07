e4e5e3c85a8b12deecf11774ff637f3447c9504d
yisus17-stack <esquivelyisus17@gmail.com>
2025-12-07 22:23:38 +0000

    quitale mas espcio entre ellas

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -227,7 +227,7 @@ function CatalogPageContent() {
                     </div>
 
                     {paginatedCars.length > 0 ? (
-                       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6">
+                       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-4">
                             {paginatedCars.map(car => (
                                 <CarCard 
                                     key={`car-${car.id}`} 
