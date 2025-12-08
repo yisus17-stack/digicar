8de8971737fa51036d14770d743fdba682627872
yisus17-stack <esquivelyisus17@gmail.com>
2025-12-08 02:29:48 +0000

    EN VERCEL TENGO ESTE ERROR:

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,3 +1,6 @@
+
+'use client';
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
@@ -7,13 +10,20 @@ export function useIsMobile() {
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
+      setIsMobile(mql.matches);
+    };
+
+    // Set initial value after mount
+    onChange();
+    
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    return () => {
+      mql.removeEventListener("change", onChange)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
