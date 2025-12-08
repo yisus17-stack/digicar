c83862c928439e407543ce9082276d7fb42f7895
yisus17-stack <esquivelyisus17@gmail.com>
2025-12-08 07:06:44 +0000

    puedes poner las card mas pegadas en movil:

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -252,7 +252,7 @@ function CatalogPageContent() {
                     </div>
 
                     {paginatedCars.length > 0 ? (
-                       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6 lg:gap-8">
                             {paginatedCars.map(car => (
                                 <CarCard 
                                     key={`car-${car.id}`} 
