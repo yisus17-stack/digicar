5218872670cc3ee1e66126c61b2bd0455b68230c
yisus17-stack <esquivelyisus17@gmail.com>
2025-12-08 08:07:07 +0000

    un poco mas lento

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        marquee: 'marquee 40s linear infinite',
+        marquee: 'marquee 60s linear infinite',
         pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
